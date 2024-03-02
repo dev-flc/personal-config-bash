4c202e740110017c2068ac36fa7451745e6974cc
{ dev : F.L.C } <20364582+dev-flc@users.noreply.github.com>
2024-03-02 15:57:12 -0600

    Update main.sh

--- src/main.sh
+++ src/main.sh
@@ -1,9 +1,10 @@
+#!/bin/sh
 
 NEW_NAME=".config-profile"
 
 eval $(dircolors -b $HOME/${NEW_NAME}/src/.dircolors)
 
 source "${HOME}/${NEW_NAME}/src/const.sh"
+source "${HOME}/${NEW_NAME}/src/funtions_git.sh"
 source "${HOME}/${NEW_NAME}/src/alias_bash.sh"
 source "${HOME}/${NEW_NAME}/src/prompt_bash.sh"
-source "${HOME}/${NEW_NAME}/src/funtions_git.sh"
