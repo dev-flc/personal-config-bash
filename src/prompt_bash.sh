84baa3ebee6a82330a836c5d9ddcc56287c777ad
{ dev : F.L.C } <20364582+dev-flc@users.noreply.github.com>
2024-03-02 20:52:53 -0600

    Update prompt_bash.sh

--- src/prompt_bash.sh
+++ src/prompt_bash.sh
@@ -1,35 +1,9 @@
-find_git_branch() {
-  local branch
-  if branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null); then
-    if [[ "$branch" == "HEAD" ]]; then
-      branch='detached*'
-    fi
-    git_branch=$branch
-  else
-    git_branch="👽"
-  fi
-}
+#!/bin/bash
 
-find_git_dirty() {
-  local status=$(git status --porcelain 2> /dev/null)
-  if [[ "$status" != "" ]]; then
-    git_dirty=" 🔥 "
-  else
-    git_dirty=" "
-  fi
-}
-PROMPT_COMMAND="find_git_branch; find_git_dirty; $PROMPT_COMMAND"
+promp_root="\${debian_chroot:+(\$debian_chroot)}"
+prompt_signature="${txtblu}┌─ 🤖 ${txtred}{ ${txtylw}dev ${txtred}: ${txtylw}flc${txtred} }"
+prompt_directory="${txtblu}[ ${txtrst}${undgrn}\w${txtrst}${txtblu} ]"
+prompt_branch="${txtblu}└──┤▶ ${txtgrn}\$(find_git_branch)${txtrst}"
+prompt_check_git="\$(check_git_changes)"
 
-
-export PS1="\${debian_chroot:+(\$debian_chroot)}\
-${txtblu}┌─${BOLD}${txtrst}${bldred}{ ${txtrst}${BOLD}${bldylw}dev : F.L.C${txtrst}${bldred} } ${bldblu}[ ${txtrst}${txtgrn}\w${txtrst}${bldblu} ]${txtrst}${BOLD}${txtrst}\n\
-${txtblu}└──┤▶${txtrst}${bldcyn} { \$git_branch }\[$txtred\]\$git_dirty\[$txtrst\]\$"
-
-#export PS1="\${debian_chroot:+(\$debian_chroot)}\
-#\[\033[38;5;208m\]┌─\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;197m\]<\
-#\[$(tput sgr0)\]\[$(tput bold)\]\[\033[38;5;226m\]FernandoLC\
-#\[$(tput sgr0)\]\[\033[38;5;197m\]/>\
-#\[\033[38;5;208m\](\[$(tput sgr0)\]\[\033[38;5;2m\]\w\[$(tput sgr0)\]\[\033[38;5;208m\])\
-#\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;1m\]\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;208m\]\n\
-#└──┤▶\[$txtblu\]\$batery\$git_branch\[$txtred\]\$git_dirty\[$txtrst\] \$"
-
+export PS1="$promp_root${BOLD}$prompt_signature$prompt_directory\n${BOLD}$prompt_branch $prompt_check_git\$ "
