1a0263087ad8e7a892b4d587d39dfdff32db2668
{ dev : F.L.C } <20364582+dev-flc@users.noreply.github.com>
2024-04-05 10:45:03 -0600

    Update main.sh

--- src/main.sh
+++ src/main.sh
@@ -1,9 +1,10 @@
+
 #!/bin/sh
-NEW_NAME=".config-profile"
+NEW_NAME=".dev-flc"
+NEW_RUTE="$HOME/$NEW_NAME"
 
 eval $(dircolors -b $HOME/${NEW_NAME}/src/utils/.config-profile-dircolors)
-
-source "${HOME}/${NEW_NAME}/src/utils/config-profile-const.sh"
-source "${HOME}/${NEW_NAME}/src/utils/config-profile-funtions.sh"
-source "${HOME}/${NEW_NAME}/src/utils/config-profile-alias.sh"
-#source "${HOME}/${NEW_NAME}/src/utils/config-profile-prompt.sh"
+source "${NEW_RUTE}/src/utils/config-profile-const.sh"
+source "${NEW_RUTE}/src/utils/config-profile-funtions.sh"
+source "${NEW_RUTE}/src/utils/config-profile-alias.sh"
+#source "${NEW_RUTE}/src/utils/config-profile-prompt.sh"
